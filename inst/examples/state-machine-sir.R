d28ecfce8659d7ea842b969a93e8f6bb152bbd62
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2020-05-15 09:12:23 -0400

    Valid states d.f.

--- inst/examples/state-machine-sir.R
+++ inst/examples/state-machine-sir.R
@@ -82,7 +82,7 @@ attr_string = function(gr, attr) {
 
 # A plot of sorts
 gr = smgr::as_graph(states)
-pdf("/tmp/sir-graph.pdf", width = 65, height = 65)
+pdf("/tmp/sir-graph.pdf", width = 100, height = 100)
 plot(gr, 
   vertex.label = paste(
     attr_string(gr, "outcome"),
@@ -95,6 +95,8 @@ plot(gr,
   ),
   vertex.shape = 'square',
   vertex.color = 'lightyellow',
-  vertex.size = 10
+  vertex.size = 7
 ); dev.off()
 
+# Valid combinations
+states$dump %>% dplyr::bind_rows()
